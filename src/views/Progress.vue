b4270da1c0c4ac65e733cffbb92229dea9c949f4
Michael McNees <mcnees.michael@gmail.com>
2021-11-04 19:58:34 -0400

    Completed basic progress page

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -1,5 +1,72 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div
+        class="
+            w-screen
+            h-screen
+            bg-gray-800
+            flex flex-col
+            items-center
+            justify-center
+            gap-8
+        "
+    >
+        <p class="text-white text-3xl">
+            ${{ Number(progress).toFixed(2) }} / ${{ Number(goal).toFixed(2) }}
+        </p>
+        <div
+            id="container"
+            class="w-4/5 bg-gray-600 rounded-full h-1/6 flex items-center p-6"
+        >
+            <div
+                id="bar"
+                :style="percentageWidth"
+                class="
+                    bg-purple-600
+                    h-full
+                    rounded-l-full
+                    flex
+                    items-center
+                    justify-center
+                "
+            >
+                <p class="text-white text-3xl">{{ percentage.toFixed(0) }}%</p>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import { ref, onBeforeMount, computed } from 'vue'
+export default {
+    setup() {
+        const refresh = 60000 // in ms (60,000 = 1 minute)
+        const participantId = 451384
+        const goal = ref(0)
+        const progress = ref(0)
+        const percentage = computed(() => (progress.value / goal.value) * 100)
+        const percentageWidth = computed(
+            () => `width: ${percentage.value.toFixed(0)}%;`
+        )
+
+        const fetchProgress = () => {
+            console.log('Fetching updated donation data...')
+            fetch(`https://extra-life.org/api/participants/${participantId}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data)
+                    goal.value = data.fundraisingGoal
+                    progress.value = data.sumDonations
+                })
+        }
+
+        onBeforeMount(() => {
+            fetchProgress()
+        })
+        setInterval(() => {
+            fetchProgress()
+        }, refresh)
+
+        return { goal, progress, percentage, percentageWidth }
+    },
+}
+</script>
