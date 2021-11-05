e530b482e71f066a87de745b036a95991800e390
Michael McNees <mcnees.michael@gmail.com>
2021-11-05 18:45:44 -0400

    Humoring Kenway with webm version

--- src/views/Alerts.vue
+++ src/views/Alerts.vue
@@ -28,7 +28,7 @@
                 </li>
             </ul>
             <video width="1920" height="1080" autoplay>
-                <source src="/HighRoller.mp4" type="video/mp4" />
+                <source src="/HighRoller.webm" type="video/webm" />
             </video>
         </div>
         <button
